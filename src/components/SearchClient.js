02ecc6eda64b7138e626038116b815b8b403a165
Compote <simbiatoff@gmail.com>
2018-09-15 18:55:58 +0300

    fixed className

--- src/components/SearchClient.js
+++ src/components/SearchClient.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class SearchClient extends Component {
   render() {
     return (
-      <div className='ui segments'>
-        <label className='ui segments'
-          htmlFor='search'>Search client</label>
-        <div class="ui icon input loading">
+      <div className=''>
+        <label className='ui header'
+          htmlFor='search'>Search client </label>
+        <div className="ui icon input loading">
           <input type="search" id="search"
             placeholder="Enter search query" />
-          <i class="search icon"></i>
+          <i className="search icon"></i>
         </div>
       </div>
     );
