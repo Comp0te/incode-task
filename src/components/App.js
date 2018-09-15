89c247771d56542ae07edc162c1fdad644ea51b8
Compote <simbiatoff@gmail.com>
2018-09-15 15:36:46 +0300

    added DetailedClient view

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import ClientList from './ClientList';
+import DetailedClient from './DetailedClient'
 
 class App extends Component {
   render() {
     return (
+      <article className='ui grid container'>
+        <nav className='six wide column'>
+          <ClientList clients={} activeClient={} />
+        </nav>
+        <article className='ten wide column'>
+          <DetailedClient client= {} />
+        </article>
+      </article>
     );
   }
 }
---
+++ src/components/DetailedClient.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+class DetailedClient extends Component {
+  render() {
+    return (
+      <div className='ui two column grid'>
+        {this.getGeneral()}
+        {this.getContact()}
+        {this.getAddress()}
+      </div>
+    )
+  }
+
+  getGeneral() {
+    const { firstName, lastName, avatar } = this.props.general
+    const { title, company } = this.props.job
+
+    return (
+      <section className='row'>
+        <div className='five wide column'>
+          <img className='ui circular image'
+            src={avatar}
+            width='128'
+            height='128'
+            alt='Client photo' />
+        </div>
+        <div className='eleven wide column'>
+          <h1 className='ui header'>
+            {firstName} {lastName}
+          </h1>
+          <h2 className='ui header'>
+            {title} - {company}
+          </h2>
+        </div>
+      </section>
+    );
+  }
+
+  getContact() {
+    const { email, phone } = this.props.contact
+
+    return (
+      <section className='row'>
+        <div className='eleven wide right floated column'>
+          <h2 className='ui header'>Contact</h2>
+          <ul className='ui list'>
+            <li>Email - {email}</li>
+            <li>Phone - {phone}</li>
+          </ul>
+        </div>
+      </section>
+    )
+  }
+
+  getAddress() {
+    const { zipCode, country, city, street } = this.props.address
+
+    return (
+      <section className='row'>
+        <div className='eleven wide right floated column'>
+          <h2 className='ui header'>Address</h2>
+          <p>{zipCode}, {country}, {city}, {street}.</p>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default DetailedClient;
