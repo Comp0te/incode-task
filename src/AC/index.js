688475d33a497447f67e61d3acd28edfbc24d10a
Compote <simbiatoff@gmail.com>
2018-09-15 23:30:50 +0300

    added selectClient actionCreator

--- src/AC/index.js
+++ src/AC/index.js
@@ -1,10 +1,16 @@
 import defaultClients from '../defaultClients';
-import {LOAD_ALL_CLIENTS} from '../constants'
+import {LOAD_ALL_CLIENTS, SELECT_CLIENT} from '../constants'
 
 export function loadClients() {
-  console.log("экшен криатор")
   return {
     type: LOAD_ALL_CLIENTS,
     payload: defaultClients
+  }
+}
+
+export function selectClient(index) {
+  return {
+    type: SELECT_CLIENT,
+    payload: index
   }
 }
