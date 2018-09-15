54bd98f3a55f70d5806187525929411c499728c3
Compote <simbiatoff@gmail.com>
2018-09-15 18:48:33 +0300

    added reduser

---
+++ src/reduser/clients.js
@@ -0,0 +1,12 @@
+import {LOAD_ALL_CLIENTS} from '../constants'
+
+export default (clientsState = [], action) => {
+  const {type} = action
+  switch (type) {
+    case LOAD_ALL_CLIENTS:
+    console.log("редусер")
+    return action.payload
+  }
+
+  return clientsState
+}
