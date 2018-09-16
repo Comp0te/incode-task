048023a4914551f532ca60d99c8aa7f1af40ecd4
Compote <simbiatoff@gmail.com>
2018-09-16 17:08:12 +0300

    added simple filter function

--- src/helper.js
+++ src/helper.js
@@ -1,30 +1,30 @@
-export const filter = (query) => {
-  return (elem) => {
-    let rez;
-    for (const key in elem) {
-      if (elem.hasOwnProperty(key)) {
-        const nestedObj = elem[key];
-        console.log('---- внешний объект')
+// export const filter = (query) => {
+//   return (elem) => {
+//     let rez = false;
+//     for (const key in elem) {
+//       if (elem.hasOwnProperty(key)) {
+//         const nestedObj = elem[key];
+//         console.log('---- внешний объект')
         
-        for (const key in nestedObj) {
-          if (nestedObj.hasOwnProperty(key)) {
-            const string = nestedObj[key];
-            console.log(key);
+//         for (const key in nestedObj) {
+//           if (nestedObj.hasOwnProperty(key)) {
+//             const string = nestedObj[key];
+//             console.log(key);
 
-            if (string.toLowerCase().trim().includes(query)) {
-              rez = true
-            } else {
-              rez = false;
-            }
+//             if (string.toLowerCase().trim().includes(query)) {
+//               rez = true
+//             } else {
+//               rez = false;
+//             }
 
-          }
-        }
-      }
-    }
-    console.log(rez)
-    return rez;
-  }
-}
+//           }
+//         }
+//       }
+//     }
+//     console.log(rez)
+//     return rez;
+//   }
+// }
 
 // const test = [{
 //   general: {
@@ -49,3 +49,46 @@ export const filter = (query) => {
 // }]
 
 // test.filter(filter("Liana"))
+
+export const filter = (query) => {
+  return (elem) => {
+    let rez = false;
+    
+    const {general, job, contact, address} = elem
+
+    // const objToArray = (obj) => {
+    //   let arr = [];
+    //   for (const key in obj) {
+    //     if (obj.hasOwnProperty(key)) {
+    //       const element = Object.values(obj[key]);
+
+    //       arr = arr.concat(element)
+    //     }
+    //   }
+    //   return arr
+    // }
+
+    // rez = objToArray(elem).reduce((accumulator, currentValue) => {
+    //   return accumulator || currentValue.includes(query);
+    // }, false);
+
+    // rez = checkObj(general, query) ||
+    //       checkObj(job, query) ||
+    //       checkObj(contact, query) ||
+    //       checkObj(address, query)
+
+    rez = general.firstName.toLowerCase().includes(query) ||
+    general.lastName.toLowerCase().includes(query) ||
+    job.company.toLowerCase().includes(query) ||
+    job.title.toLowerCase().includes(query) ||
+    contact.email.toLowerCase().includes(query) ||
+    contact.phone.toLowerCase().includes(query) ||
+    address.street.toLowerCase().includes(query) ||
+    address.city.toLowerCase().includes(query) ||
+    address.zipCode.toLowerCase().includes(query) ||
+    address.country.toLowerCase().includes(query) ||
+
+    console.log(rez)
+    return rez;
+  }
+}
