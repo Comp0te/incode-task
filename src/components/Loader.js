6f2466e4bdcd9a9c9e92fc695fb1fc54ba30c069
Compote <simbiatoff@gmail.com>
2018-09-16 18:49:56 +0300

    went away:-)

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Loader() {
   return (
     <div className="ui active inverted dimmer">
-      <div className="ui medium centered text loader">Preparing Files</div>
+      <div className="ui medium centered text loader">Preparing Files...</div>
     </div>
   )
 }
