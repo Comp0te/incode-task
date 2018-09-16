62e64b42571b44db59f073df42b294f37182c954
Compote <simbiatoff@gmail.com>
2018-09-16 18:46:20 +0300

    added new case of action.type

--- src/reduser/clients.js
+++ src/reduser/clients.js
@@ -1,18 +1,37 @@
-import { LOAD_ALL_CLIENTS, SELECT_CLIENT, FILTER_CLIENTS } from '../constants'
+import { LOAD_ALL_CLIENTS_START, LOAD_ALL_CLIENTS_SUCCESS, LOAD_ALL_CLIENTS_FAIL, SELECT_CLIENT, FILTER_CLIENTS } from '../constants'
 
 const defaultClientsState = {
   clientsData: [],
   activeClient: null,
+  loading: false,
+  loaded: false,
+  errorLoadMessage: ''
 }
 
 export default (clientsState = defaultClientsState, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case LOAD_ALL_CLIENTS: {
+    case LOAD_ALL_CLIENTS_START: {
       return {
         ...clientsState,
-        clientsData: [].concat(payload)
+        loading: true
+      }
+    }
+
+    case LOAD_ALL_CLIENTS_SUCCESS: {
+      return {
+        ...clientsState,
+        clientsData: payload,
+        loading: false,
+        loaded: true
+      }
+    }
+
+    case LOAD_ALL_CLIENTS_FAIL: {
+      return {
+        ...clientsState,
+        errorLoadMessage: payload
       }
     }
 
