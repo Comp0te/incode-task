e21f2ec2f2ed8313dd8c53f232b75ea9ee7c434e
Compote <simbiatoff@gmail.com>
2018-09-16 18:43:36 +0300

    rewokked loadClients into asynchronous download function from the server

--- src/AC/index.js
+++ src/AC/index.js
@@ -1,10 +1,41 @@
 import defaultClients from '../defaultClients';
-import {LOAD_ALL_CLIENTS, SELECT_CLIENT, FILTER_CLIENTS} from '../constants'
+import {SELECT_CLIENT, FILTER_CLIENTS, LOAD_ALL_CLIENTS_START, LOAD_ALL_CLIENTS_SUCCESS, LOAD_ALL_CLIENTS_FAIL} from '../constants'
 
-export function loadClients() {
-  return {
-    type: LOAD_ALL_CLIENTS,
-    payload: defaultClients
+export function loadClients(url) {
+  return (dispatch) => {
+    dispatch({
+      type: LOAD_ALL_CLIENTS_START,
+    })
+
+    setTimeout(() => {
+      fetch(url, {
+        method: 'GET',
+        mode: "same-origin",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          
+          return defaultClients;
+          // return response.json();
+        })
+        .then((response) => {
+          dispatch({
+            type: LOAD_ALL_CLIENTS_SUCCESS,
+            payload: response
+          })
+        })
+        .catch((error) => {
+          dispatch({
+            type: LOAD_ALL_CLIENTS_FAIL,
+            payload: error.message
+          })
+        })
+    }, 1000)
   }
 }
 
