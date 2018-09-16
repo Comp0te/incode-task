c01bfed87d9973dca287fee1e5aaa34af36bcc36
Compote <simbiatoff@gmail.com>
2018-09-16 14:31:00 +0300

    added connecn to store

--- src/components/SearchClient.js
+++ src/components/SearchClient.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { filterClients } from '../AC'
 
 class SearchClient extends Component {
   render() {
+
     return (
-      <div className=''>
+      <div>
         <label className='ui header'
           htmlFor='search'>Search client </label>
         <div className="ui icon input loading">
           <input type="search" id="search"
-            placeholder="Enter search query" />
+            placeholder="Enter search query..."
+            onChange={this.onSearch} />
           <i className="search icon"></i>
         </div>
       </div>
     );
   }
+
+  onSearch = (evt) => {
+    const searchValue = evt.target.value.toLowerCase().trim();
+    console.log('---------', searchValue)
+    this.props.filterClients(searchValue)
+  }
 }
 
-export default SearchClient;
+// const mapStateToProps = (state) => {
+//   return {
+//     data: state.clients.clientsData
+//   }
+// }
+
+const mapDispatchToProps = (dispatch) => (
+  {
+    filterClients: (searchQuery) => {
+      dispatch(filterClients(searchQuery))
+    }
+  }
+);
+
+export default connect(null, mapDispatchToProps)(SearchClient);
